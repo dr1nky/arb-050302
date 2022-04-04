e9f71960417613099b365b313c811fe9013c792d
dr1nky <leedrinkwater@hotmail.com>
2022-04-04 19:20:05 +0100

    added leafletjs

--- main.js
+++ main.js
@@ -45,3 +45,33 @@ new Glide('.glide').mount()
 
 // baquetteBoxJs Images
 baguetteBox.run('.gallery-one')
+
+// LeafletJS
+const lat = 53.19154575419936;
+// const lon = pos.coords.longitude;
+const lon = -2.534168895042527;
+// const time = pos.timestamp;
+
+// 53.19154575419936, -2.534168895042527
+
+// leaflet
+var mymap = L.map('mapid').setView([lat, lon], 15);
+const attribution = 'Open Street Map';
+
+const tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(mymap);
+
+// var greenIcon = new L.Icon({
+//   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+//   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+//   iconSize: [25, 41],
+//   iconAnchor: [12, 41],
+//   popupAnchor: [1, -34],
+//   shadowSize: [41, 41]
+// });
+
+var marker = L.marker([lat, lon]).addTo(mymap);
+const txt = `1a Alexandra Street, Winsford CW7 2DS, UK <a target="_blank" href="https://www.google.com/maps/dir/?api=1&destination=1a%20Alexandra%20Street,%20Winsford%20CW7%202DS,%20UK">Directions</link>`;
+
+marker.bindPopup(txt);
